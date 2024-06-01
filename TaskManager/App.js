82defc68fb4895df82fcc53ef4ec154e59cefcfd
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-06-01 00:45:58 +0000

    added categories section

--- TaskManager/App.js
+++ TaskManager/App.js
@@ -1,12 +1,14 @@
 import { StyleSheet,View } from 'react-native';
 import Header from './components/Header';
-import Search from './components/Search'
+import Search from './components/Search';
+import Categories from './components/Categories';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <Header/>
      <Search/>
+     <Categories/>
     </View>
   );
 }
--- TaskManager/components/Categories.jsx
+++ TaskManager/components/Categories.jsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image} from 'react-native'
 import React from 'react'
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Categories</Text>
+      <View style={styles.categories}>
+        <Text style={styles.exercise}>Exercise</Text>
+        <Text style={styles.task}>12 Tasks</Text>
+        <Image source={require('../assets/images/exercise.png')} style={styles.categoryImg} resizeMode='contain'/>
+      </View>
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    top:50,
+    left:20,
+  },
+  title:{
+    fontSize:20,
+    fontWeight:700,
+  },
+  categories:{
+    top:15,
+    width:186,
+    height:195,
+    backgroundColor:'#FBF9F7',
+    borderRadius:15,
+  },
+  exercise:{
+    fontSize:16,
+    left:16,
+    marginTop:10,
+    fontWeight:700,
+  },
+  task:{
+    marginTop:3,
+    fontSize:12,
+    left:16,
+    fontWeight:400,
+  },
+  categoryImg:{
+    height:140,
+    top:5,
+    right:130,
+  },
+})
